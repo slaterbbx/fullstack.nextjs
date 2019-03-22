8cefde19e63620cbe896411d11744a78db1d7c70
Kyle Gallagher <litonfiredesign@gmail.com>
2019-03-22 17:34:06 -0400

    added some comments

--- client/store/sagas/saga.ts
+++ client/store/sagas/saga.ts
@@ -1,6 +1,5 @@
 import { put } from 'redux-saga/effects';
 import es6promise from 'es6-promise'
-// import axios from 'axios'
 
 import {
     countIncrement,
@@ -9,10 +8,14 @@ import {
 
 es6promise.polyfill()
 
-export function* delayedIncrement(amount) {
+export function* delayedIncrement(amount: number) {
+	// We are using the saga to pass the countIncremenent payload back to itself
+	// This is only to show that the Sagas are functioning.
     put(countIncrement(amount))
 }
 
-export function* delayedDecrement(amount) {
+export function* delayedDecrement(amount: number) {
+	// We are using the saga to pass the countDecrement payload back to itself
+	// This is only to show that the Sagas are functioning.
     put(countDecrement(amount))
 }
