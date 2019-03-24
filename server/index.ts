184a48a4de7e40e099da78fbb394f410533133ac
Kyle Gallagher <litonfiredesign@gmail.com>
2019-03-24 00:18:44 -0400

    small changes

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,10 @@
 const express = require('express');
-const next = require('next');
+import * as next from 'next';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dir: './client', dev });
 const handle = app.getRequestHandler();
 const port = parseInt(process.env.PORT, 10) || 3000;
-
-
 
 app.prepare().then(() => {
 
