ad24d9c8c6f6bf8973e2a0c301997dfeb8f7dd53
Kyle Gallagher <litonfiredesign@gmail.com>
2019-03-24 00:34:09 -0400

    todo

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,10 @@
 const express = require('express');
 import * as next from 'next';
 
+// const bodyParser = require('body-parser');
+// const graphqlHttp = require('express-graphql');
+// const { buildSchema } = require('graphql');
+
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dir: './client', dev });
 const handle = app.getRequestHandler();
@@ -9,6 +13,25 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 app.prepare().then(() => {
 
 	const server = express();
+	
+	// server.use(bodyParser.json());
+	// server.use('/graphql', graphqlHttp({
+	// 	schema: buildSchema(`
+	// 		type RootQuery {
+				
+	// 		}
+
+	// 		type RootMutation {
+
+	// 		}
+
+	// 		schema {
+	// 			query: RootQuery,
+	// 			mutation: RootMutation
+	// 		}
+	// 	`),
+	// 	rootValue: {}
+	// }))
     
 	server.get('*', (req, res) => {
 		return handle(req, res);
