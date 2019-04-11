fc09935b136b5e50980cd78786482b3bb98b36db
Kyle Gallagher <litonfiredesign@gmail.com>
2019-04-11 10:22:28 -0400

    removed express-graphql in favor of apollo

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,6 @@
 import * as express from 'express';
 import * as next from 'next';
 
-// import bodyParser from 'body-parser';
-import * as graphqlHttp from 'express-graphql';
-import { buildSchema } from 'graphql';
-
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dir: './client', dev });
 const handle = app.getRequestHandler();
@@ -13,35 +9,6 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 app.prepare().then(() => {
 
 	const server = express();
-	
-	// All graphQL stuff is working but I will add examples in future updates
-	// server.use(bodyParser.json());
-	server.use('/graphql', graphqlHttp({
-		schema: buildSchema(`
-			type RootQuery {
-				savedNumbers: [Int!]
-			}
-
-			type RootMutation {
-				addNumber: Int
-			}
-
-			schema {
-				query: RootQuery,
-				mutation: RootMutation
-			}
-		`),
-		rootValue: {
-			savedNumbers: () => {
-				return [10, 12, 23];
-			},
-			addNumber: (number) => {
-				return number;
-			}
-		},
-		// dev only
-		graphiql: true
-	}))
     
 	server.get('*', (req, res) => {
 		return handle(req, res);
