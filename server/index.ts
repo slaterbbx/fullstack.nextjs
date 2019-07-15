341164d3054e9f27854c2c63f6d369e391b16731
Kyle Gallagher <litonfiredesign@gmail.com>
2019-07-15 08:10:03 -0400

    added conditional for server port consol log

--- server/index.ts
+++ server/index.ts
@@ -16,7 +16,7 @@ app.prepare().then(() => {
 
 	server.listen(port, (err) => {
 		if (err) throw err;
-		console.log(`> Ready on http://localhost:${port}`);
+		if (dev) console.log(`> Ready on http://localhost:${port}`);
 	})
 	
 })
