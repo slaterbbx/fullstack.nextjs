ac50673cd7cd03844d945997015f8e23aef17a71
Kyle Gallagher <litonfiredesign@gmail.com>
2019-07-18 12:32:07 -0400

    removed old dependants

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,4 @@
-// const withPlugins = require('next-compose-plugins');
-// const withTypescript = require('@zeit/next-typescript');
-
-
 // next.js configuration
 const nextConfig = {
 	distDir: '../dist/client',
 };
-  
-// module.exports = withPlugins([
-// 	[withTypescript(), {}],
-// ], nextConfig);
