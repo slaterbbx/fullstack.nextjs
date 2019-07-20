42aec4d794c464c65b73535f88044da9d6aa1cc0
Kyle Gallagher <litonfiredesign@gmail.com>
2019-07-20 03:27:44 -0400

    removed unused code

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -7,22 +7,13 @@ import withReduxSaga from 'next-redux-saga';
 import makeStore from '../store/store';
 
 class MyApp extends App {
-  static async getInitialProps ({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps({ ctx })
-    }
-
-    return { pageProps }
-  }
 
   render () {
-    const { Component, pageProps, store } = this.props
+    const { Component, store } = this.props
     return (
       <Container>
         <Provider store={store}>
-          <Component {...pageProps} />
+          <Component/>
         </Provider>
       </Container>
     )
